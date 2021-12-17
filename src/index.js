18a2b01a4f67c9af8e70b6d0c86983788dde338d
Devorris Russell <devorris_r@yahoo.com>
2021-12-17 14:19:02 -0500

    added imports to index.js files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
+import { Browser as Router } from 'react-router-dom'
+
+
+ReactDom.render(
+    <Router>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Router>,
+document.getElementById('root')
+);

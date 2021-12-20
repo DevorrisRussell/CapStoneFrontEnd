d39fc319182bdab5928fe45752d946c36a509fd1
Devorris Russell <devorris_r@yahoo.com>
2021-12-19 19:49:21 -0500

    added route paths and have to futher update them

--- src/app.jsx
+++ src/app.jsx
@@ -20,7 +20,7 @@ class App extends Component {
                     user: null,
                 };
             }
-        }
+        } 
         componentDidMount() {
             const jwt = localStorage.getItem("token");
             try{
@@ -34,9 +34,41 @@ class App extends Component {
             let user = this.state.user;
             console.log(user);
             return (
-                
-            )
-        }
-
-    }
-}
+              <div>
+                <TopBar user={user} />
+                <div>
+                {/* <div className="card" style={{width: "18rem"}}>
+                     <img src="..." className="card-img-top" alt="..."/>
+                     <div className="card-body">
+                       <h5 clasNames="card-title">Card title</h5>
+                       <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                       <a href="#" className="btn btn-primary">Go somewhere</a>
+                     </div>
+                    </div> */}
+                  <Switch>
+                    <Route
+                      path="/"
+                      exact
+                      render={(props) => {
+                        if (!user) {
+                          return <Redirect to="/register" />;
+                        } else {
+                          return <Home {...props} user={user} />;
+                        }
+                      }}
+                    />
+                    <Route path="/register" component={Register} />
+                    <Route path="/Login" component={Login} />
+                    <Route path="/logout" component={LogOut} user={user} />
+                    <Route path="/allfriends" component={Friends} />
+                    {/* <Route path="/not-found" component={NotFound} /> */}
+                    {/* <Redirect to="/not-found"/> */}
+                  </Switch>
+                </div>
+              </div>
+            );
+          }
+            }}
+          
+export default App;
+    

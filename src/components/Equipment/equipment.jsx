6234f557b65daf4bdd30c7ca484db2984d2955ba
Devorris Russell <devorris_r@yahoo.com>
2021-12-28 15:13:26 -0500

    adding files to home

--- src/components/Equipment/equipment.jsx
+++ src/components/Equipment/equipment.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./equipment.css";
+import { propertiesParser } from "config/parser";
 //import { propertiesParser } from "config/parser";
 
 
@@ -44,27 +45,47 @@ function Equipment(){
   // }, [newEquipment]);
 
     return (
+     
+   
       <div>
       <h1> Equipment List </h1>
       <div className="Comment">
         <form onSubmit={(e) => handleSubmit(e)}>
+          {equipmentId.map((equipmentId) => (
+            <div className=" form-inline" key={Equipment}>
+              </div>
+          )
+          )}
           <div className="card">
             <div className="card-header"></div>
             <div class="card-body">
               <blockquote className="blockquote mb-0">
                 <div className="form-group" size="lg" controlId="name">
                   <label className="form-label">
+                  
                   <button onSubmit={(e) => handleSubmit(e)}>Add Equipment</button>
-                  <table class="table">
-  <thead>
+                  
+                  <button onSubmit={(e) => handleSubmit(e)}>Borrow Equipment</button>
+                  
+                  
+
+                  {/* <table class="table"> */}
+  {/* <thead>
     <tr>
       <th scope="col">Name</th>
       <th scope="col">Description</th>
       
     </tr>
   </thead>
-  <tbody></tbody>
-                   </table> 
+  <tbody>
+    {Equipment?.map((Equipment) => (
+      <tr>
+        <li key=   {Equipment.name}> </li>
+      </tr>
+    )
+    )}
+  </tbody>
+                   </table>  */}
          </label>
                 </div>
                 
@@ -77,7 +98,7 @@ function Equipment(){
       
       
      
-    )
+      )
     }
 
 export default Equipment;

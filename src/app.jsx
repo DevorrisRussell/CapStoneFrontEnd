867f52afd731e8a902386e4703d26ea0f59a3fb2
Devorris Russell <devorris_r@yahoo.com>
2021-12-29 12:43:37 -0500

    adding new myequipment for home screen

--- src/app.jsx
+++ src/app.jsx
@@ -3,11 +3,11 @@ import React, { Component } from "react";
 
 import jwt_decode from "jwt-decode";
 
-import Home from "./components/Home/home";
+import MyEquipment from "./components/MyEquipment/myEquipment";
 import Register from "./components/Register/register";
 import Login from "./components/Login/login"
 import NavigationBar from "./components/NavigationBar/navigationBar";
-import Equipment from "./components/Equipment/equipment";
+// import Equipment from "./components/Equipment/equipment";
 import Logout from "./components/Logout/logout"
 
 
@@ -59,16 +59,16 @@ class App extends Component {
               if (!user) {
                 return < Redirect to="/register" />;
             } else {
-              return < Equipment {...props} user={user} />;
+              return < Register {...props} user={user} />;
             }
             } }
             />
 
              <Register path="/Register" component={Register} />
              < Login path="/Login" component={Login} /> 
-            <Home path='/Home' component={Home} /> 
+            <MyEquipment path='/MyEquipment' component={MyEquipment} /> 
              <NavigationBar path="/NavigationBar" component={NavigationBar} />
-             < Equipment path="/Equipment" component={Equipment} />
+             {/* < Equipment path="/Equipment" component={Equipment} /> */}
              < Logout path="/Logout" component={Logout} user={user} />
              
 
---
+++ src/components/MyEquipment/myEquipment.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import {Link} from "react-router-dom"
+
+function MyEquipment(){
+    const [myEquipment, setMyEquipment] = useState([])
+
+   useEffect(() => {
+      getAllMyEquipment();
+   }, [])
+
+
+    async function getAllMyEquipment() {
+        const jwt = localStorage.getItem('token');
+        let configObject = {
+            headers: {
+                'x-auth-token': jwt
+            }
+        } 
+        let response = await axios.get(`http://localhost:5000/api/users/myList`, configObject)
+        setMyEquipment(response.data)
+        console.log(response.data)
+    }
+
+
+
+    return(
+        <div>
+            <Link to='/'>Home</Link>
+          
+            {myEquipment.length > 0 &&
+              myEquipment.map(Equipments => <h1>{Equipments.name} </h1> )  
+            }
+        
+           
+        </div>
+    );
+}
+export default MyEquipment;

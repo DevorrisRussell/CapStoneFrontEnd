d31d96e81bafbfc32845277309af247ad11457d7
Devorris Russell <devorris_r@yahoo.com>
2022-01-04 12:04:59 -0500

    adding field for color and Serial Number

--- src/components/AddEquipment/addEquipment.jsx
+++ src/components/AddEquipment/addEquipment.jsx
@@ -50,19 +50,42 @@ function AddEquipment() {
               value={description}
               onChange={(e) => setDescription(e.target.value)}
             ></textarea>
-          </div>
-        </div>
-        <div class="d-grid gap-2 col-1 mx-auto float-right">
-          <button class="btn btn-secondary" type="submit">
-            <h3>Add</h3>
-          </button>
-        </div>
 
-        <div class="col-md-3">
-          <img
-            src="https://media.istockphoto.com/photos/using-lawn-mower-picture-id96926811?b=1&k=20&m=96926811&s=170667a&w=0&h=lFvhJ-TI2Tak4dHryZrSeLMcJxPs1qL-lCBOxYdo-Ao="
-            class="rounded float-start"
-          ></img>
+            <div className="form-group" size="lg" controlId="Name">
+              <label className="form-label">
+                <h2>Color</h2>
+              </label>
+              <input
+                autoFocus
+                type="text"
+                value={color}
+                onChange={(e) => setColor(e.target.value)}
+              />
+              <div className="form-group" size="lg" controlId="Name">
+                <label className="form-label">
+                  <h2>Serial Number</h2>
+                </label>
+                <input
+                  autoFocus
+                  type="text"
+                  value={serialNumber}
+                  onChange={(e) => setserialNumber(e.target.value)}
+                />
+              </div>
+            </div>
+            <div class="d-grid gap-2 col-1 mx-auto float-right">
+              <button class="btn btn-secondary" type="submit">
+                <h3>Add</h3>
+              </button>
+            </div>
+
+            <div class="col-md-3">
+              <img
+                src="https://media.istockphoto.com/photos/using-lawn-mower-picture-id96926811?b=1&k=20&m=96926811&s=170667a&w=0&h=lFvhJ-TI2Tak4dHryZrSeLMcJxPs1qL-lCBOxYdo-Ao="
+                class="rounded float-start"
+              ></img>
+            </div>
+          </div>
         </div>
       </form>
     </div>
